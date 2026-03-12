4a52cbcb2c5723b65613dbdeacd1e193dd21b8b4
Guilherme Oliveira <san835117@gmail.com>
2026-03-12 13:08:30 -0300

    feat:add call-to-action button to section schema for admin configuration

---
+++ assets/slider-home.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+ 
+
+  const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  });
+});
