23fa468bd500b063e7aee1efd0ea021f66f45c27
Guilherme Oliveira <san835117@gmail.com>
2026-03-17 01:27:20 -0300

    feat: add dynamic styling to product page content (name, price, and description)

--- assets/slider-collection.js
+++ assets/slider-collection.js
@@ -3,14 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const swiper = new Swiper('.colletion-container', {
         slidesPerView: 3,
-        spaceBetween: 20,
+        spaceBetween: 5,
         direction: 'horizontal',
         loop: true,
-
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
 
         navigation: {
             nextEl: '.swiper-button-next',
